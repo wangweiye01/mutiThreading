d2c6c10657a38962ad37c9250993deffb043e9b5
wangweiye <wwyknight@163.com>
2017-08-05 13:46:47 +0800

    init project

---
+++ src/PrintABC.java
@@ -0,0 +1,49 @@
+public class PrintABC implements Runnable {
+
+	private String name;
+	private Object prev;
+	private Object self;
+
+	private PrintABC(String name, Object prev, Object self) {
+		this.name = name;
+		this.prev = prev;
+		this.self = self;
+	}
+
+	@Override
+	public void run() {
+		int count = 10;
+		while (count > 0) {
+			synchronized (prev) {
+				synchronized (self) {
+					System.out.print(name);
+					count--;
+
+					self.notify();
+				}
+				try {
+					prev.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		Object a = new Object();
+		Object b = new Object();
+		Object c = new Object();
+		PrintABC pa = new PrintABC("A", c, a);
+		PrintABC pb = new PrintABC("B", a, b);
+		PrintABC pc = new PrintABC("C", b, c);
+
+		new Thread(pa).start();
+		Thread.sleep(100); // 确保按顺序A、B、C执行
+		new Thread(pb).start();
+		Thread.sleep(100);
+		new Thread(pc).start();
+		Thread.sleep(100);
+	}
+}
