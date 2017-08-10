9d475ddd02b3ef6803c1ed01083f97f1c99d998b
wangweiye <wwyknight@163.com>
2017-08-10 09:34:10 +0800

    fix

---
+++ src/com/wang/ProducerConsumerWithQueue.java
@@ -0,0 +1,47 @@
+package com.wang;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class ProducerConsumerWithQueue {
+	private int queueSize = 10;
+	private ArrayBlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(queueSize);
+
+	public static void main(String[] args) {
+		ProducerConsumerWithQueue test = new ProducerConsumerWithQueue();
+		Producer producer = test.new Producer();
+		Consumer consumer = test.new Consumer();
+
+		producer.start();
+		consumer.start();
+	}
+
+	class Consumer extends Thread {
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					queue.take();
+					System.out.println("从队列取走一个元素，队列剩余" + queue.size() + "个元素");
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	class Producer extends Thread {
+
+		@Override
+		public void run() {
+			while (true) {
+				try {
+					queue.put(1);
+					System.out.println("向队列取中插入一个元素，队列剩余空间：" + (queueSize - queue.size()));
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
